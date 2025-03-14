3b46a37918c696fc1d4a1a5a4221aebb708c2bfa
Carmen Alberte Nielsen <alcn@itu.dk>
2025-03-14 14:20:43 +0100

    implement minimax search with max depth

--- OthelloAIHotello.java
+++ OthelloAIHotello.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 
 /**
  * A simple OthelloAI-implementation. The method to decide the next move just
@@ -8,15 +7,75 @@ import java.util.ArrayList;
  */
 public class OthelloAIHotello implements IOthelloAI{
 
-	/**
-	 * Returns first legal move
-	 */
+	private int depth;
+
+	private class Decision {
+		public final int utility;
+		public final Position move;
+
+		public Decision(int utility, Position move) {
+			this.utility = utility;
+			this.move = move;
+		}
+	}
+	
 	public Position decideMove(GameState s){
-		ArrayList<Position> moves = s.legalMoves();
-		if ( !moves.isEmpty() )
-			return moves.get(0);
-		else
-			return new Position(-1,-1);
+		Decision d = maxValue(s);
+		depth = 0;
+		return d.move;
+	}
+
+	private Decision maxValue(GameState s) {
+		depth++;
+		if (s.isFinished() || depth > 20) {
+			return new Decision(utility(s), null);
+		}
+
+		int v = Integer.MIN_VALUE;
+		Position maxMove = null;
+
+		for (Position move : s.legalMoves()) {
+			GameState resultState = new GameState(s.getBoard(), s.getPlayerInTurn());
+			resultState.insertToken(move);
+
+			int v2 = minValue(resultState).utility;
+			if (v2 > v) {
+				v = v2;
+				maxMove = move;
+			}
+		}
+		return new Decision(v, maxMove);
+	}
+
+	private Decision minValue(GameState s) {
+		if (s.isFinished() || depth > 20) {
+			return new Decision(utility(s), null);
+		}
+
+		int v = Integer.MAX_VALUE;
+		Position minMove = null;
+
+		for (Position move : s.legalMoves()) {
+			GameState resultState = new GameState(s.getBoard(), s.getPlayerInTurn());
+			resultState.insertToken(move);
+
+			int v2 = maxValue(resultState).utility;
+			if (v2 < v) {
+				v = v2;
+				minMove = move;
+			}
+		}
+		return new Decision(v, minMove);
+	}
+
+	private int utility(GameState s) {
+		int[] tokens = s.countTokens();
+		int player = s.getPlayerInTurn();
+		int opponent = player % 2 + 1;
+		int playerTokens = tokens[player - 1];
+		int opponentTokens = tokens[opponent - 1];
+		return playerTokens - opponentTokens;
 	}
 	
 }
+
